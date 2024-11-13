833d47452ff553c6e10e9c6d09acfffc9f3cce28
JuanEdu117 <juaned.jimenez99@gmail.com>
2024-11-12 22:50:31 -0600

    invent dal

---
+++ DAL/Interfase/IInvent_DAL.cs
@@ -0,0 +1,16 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfase
+{
+    public interface IInvent_DAL
+    {
+        List<cls_Inventario> ConsultarInventario(cls_Inventario Obj_Entidad);
+        bool AlmacenarInventario(cls_Inventario Obj_Entidad);
+        bool EliminarInventario(cls_Inventario Obj_Entidad);
+    }
+}
---
+++ DAL/SQLServer/cls_Invent_DAL.cs
@@ -0,0 +1,69 @@
+﻿using Dapper;
+using Entidades;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.SQLServer
+{
+    public class cls_Invent_DAL
+    {
+        #region VARIABLE PRIVADA
+        private readonly IConfiguration _IConfiguracion;
+        #endregion
+
+        #region CONSTRUCTOR
+        public cls_Invent_DAL(IConfiguration iConfig)
+        {
+            _IConfiguracion = iConfig;
+        }
+        #endregion
+
+        #region METODOS
+
+        #region INVENT
+        public List<cls_Inventario> ConsultarInventario(cls_Inventario Obj_Entidad)
+        {
+            DynamicParameters parametros = new DynamicParameters();
+            parametros.Add("@FILTRO", Obj_Entidad.IdInventario, DbType.Int32, ParameterDirection.Input);
+
+            using (var CNXSQL = new SqlConnection(_IConfiguracion.GetConnectionString("ConexionSQLServer")))
+            {
+                return (List<cls_Inventario>)CNXSQL.Query<cls_Inventario>("dbo.Sp_ConsultarInventario", parametros, commandType: CommandType.StoredProcedure);
+            }
+        }
+        public bool AlmacenarInventario(cls_Inventario Obj_Entidad)
+        {
+            DynamicParameters parametros = new DynamicParameters();
+            parametros.Add("@invent_id", Obj_Entidad.IdInventario, DbType.Int32, ParameterDirection.Input);
+            parametros.Add("@descrip", Obj_Entidad.Descripcion, DbType.String, ParameterDirection.Input, 50);
+            parametros.Add("@maquina", Obj_Entidad.TipoMaquina, DbType.String, ParameterDirection.Input, 50);
+            parametros.Add("@h_actual", Obj_Entidad.HorasActuales, DbType.Decimal, ParameterDirection.Input);
+            parametros.Add("@h_max", Obj_Entidad.HorasMaximas, DbType.Decimal, ParameterDirection.Input);
+            parametros.Add("@h_mante", Obj_Entidad.HorasMantenimiento, DbType.Decimal, ParameterDirection.Input);
+
+            using (var CNXSQL = new SqlConnection(_IConfiguracion.GetConnectionString("ConexionSQLServer")))
+            {
+                return CNXSQL.Execute("dbo.Sp_AlmacenarInventario", parametros, commandType: CommandType.StoredProcedure) > 0;
+            }
+        }
+        public bool EliminarInventario(cls_Inventario Obj_Entidad)
+        {
+            DynamicParameters parametros = new DynamicParameters();
+            parametros.Add("@invent_id", Obj_Entidad.IdInventario, DbType.Int32, ParameterDirection.Input);
+
+            using (var CNXSQL = new SqlConnection(_IConfiguracion.GetConnectionString("ConexionSQLServer")))
+            {
+                return CNXSQL.Execute("dbo.Sp_EliminarInventario", parametros, commandType: CommandType.StoredProcedure) > 0;
+            }
+        }
+        #endregion
+
+        #endregion
+    }
+}
